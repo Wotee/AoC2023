1fc21c457e030ebfb16379eaac78d4f2045fcf9e
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2023-12-03 09:00:05 +0200

    Slight cleanup and perf optimization

--- Day03.fsx
+++ Day03.fsx
@@ -1,10 +1,7 @@
 let input = System.IO.File.ReadAllLines("inputs/03.txt")
 #time
 
-let arr =
-    input
-    |> Array.map Seq.toArray
-    |> array2D
+let arr = input |> array2D
 
 let getLocations f =
     arr
@@ -13,62 +10,55 @@ let getLocations f =
     |> Seq.choose id
     |> Seq.toList
 
-let rec buildNumber (locations : list<int*int*char>) =
-    [
+let rec buildNumberWithCoordinates (locations : list<int*int*char>) =
+    [|
         match locations with
         | [] -> ()
         | (x1, y1, v1)::(x2, y2, v2)::(x3,y3,v3)::rest when x1 = x2 && x2 = x3 && y1 + 1 = y2 && y2 + 1 = y3->
             yield (int $"{v1}{v2}{v3}", [(x1, y1); (x2, y2); (x3, y3)])
-            yield! buildNumber rest
+            yield! buildNumberWithCoordinates rest
         | (x1, y1, v1)::(x2, y2, v2)::rest when x1 = x2 && y1 + 1 = y2 ->
             yield (int $"{v1}{v2}", [(x1, y1); (x2, y2)])
-            yield! buildNumber rest
+            yield! buildNumberWithCoordinates rest
         | (x1, y1, v1)::rest ->
             yield (int $"{v1}", [(x1, y1)])
-            yield! buildNumber rest
-    ]
+            yield! buildNumberWithCoordinates rest
+    |]
 
 let getNeighbors (x, y) =
-    [ x - 1, y - 1; x - 1, y; x - 1, y + 1; x, y - 1; x, y + 1; x + 1, y - 1; x + 1, y; x + 1, y + 1 ]
-
-
+    [| x - 1, y - 1; x - 1, y; x - 1, y + 1; x, y - 1; x, y + 1; x + 1, y - 1; x + 1, y; x + 1, y + 1 |]
 
 let maxX = Array2D.length1 arr - 1
 let maxY = Array2D.length2 arr - 1
-let filter (locations : (int*int) list) =
-    let validNeighbors =
-        locations
-        |> List.collect getNeighbors
-        |> List.distinct
-        |> List.filter (fun (x, y) -> x >= 0 && x <= maxX && y >= 0 && y <= maxY)
-    validNeighbors
-    |> List.exists (fun (x, y) ->
+let locationCloseToSymbol (locations : (int*int) list) =
+    locations
+    |> List.toArray
+    |> Array.collect getNeighbors
+    |> Array.distinct
+    |> Array.filter (fun (x, y) -> x >= 0 && x <= maxX && y >= 0 && y <= maxY)
+    |> Array.exists (fun (x, y) ->
         let char = Array2D.get arr x y
         not (System.Char.IsDigit char || char = '.')
     )
 
-let numbers = getLocations System.Char.IsDigit |> buildNumber
+let numbers = getLocations System.Char.IsDigit |> buildNumberWithCoordinates
 
 numbers
-|> List.filter (fun (_, locations) -> filter locations)
-|> List.sumBy fst
+|> Array.filter (snd >> locationCloseToSymbol)
+|> Array.sumBy fst
 |> printfn "Part 1: %i"
-
-let gearLocations = getLocations (fun c -> c = '*')
 
 let numbersMapped =
     numbers
-    |> List.collect(fun (value, locations) -> locations |> List.map (fun (x, y) -> ((x, y), value)))
-    |> Map.ofList
+    |> Array.collect(fun (value, locations) -> locations |> List.toArray |> Array.map (fun (x, y) -> ((x, y), value)))
+    |> Map.ofArray
 
 let getGearRatios (x, y, _) =
-    let gearNeighbors = getNeighbors (x, y)
-    let gears = gearNeighbors |> List.choose (fun coord -> Map.tryFind coord numbersMapped) |> List.distinct
-    match gears with
-    | a::b::[] -> a*b
+    match getNeighbors (x,y) |> Array.choose (fun coord -> Map.tryFind coord numbersMapped) |> Array.distinct with
+    | [|a;b|] -> a*b
     | _ -> 0
 
-gearLocations
+getLocations ((=) '*')
 |> List.sumBy getGearRatios
 |> printfn "Part 2: %i"
 #time
