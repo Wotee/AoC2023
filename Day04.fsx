a7a851a3501da99b644d7a00d41d92b73b679826
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2023-12-04 11:40:57 +0200

    Cleanup

--- Day04.fsx
+++ Day04.fsx
@@ -1,18 +1,18 @@
 let input = System.IO.File.ReadAllLines("inputs/04.txt")
 #time
 
-let getNumberOfWinningNumbers (x: string) =
-    let split = x.Split([|"Card "; ":"; "|"; " "|], System.StringSplitOptions.RemoveEmptyEntries) |> Array.tail |> Array.map int
-    let winning, my = split |> Array.splitAt 10 // Ugly hack by looking at the input
-    Set.intersect (winning |> Set.ofArray) (my |> Set.ofArray)
+let getNumberOfWinningNumbersNew (x: string) =
+    let split = x.Split([|"Card"; ":"; " "|], System.StringSplitOptions.RemoveEmptyEntries) |> Array.tail
+    let winning, my = split |> Array.splitAt (split |> Array.findIndex ((=) "|"))
+    Set.intersect (set winning) (set my)
     |> Set.count
 
 let numberOfWinsPerCard = 
     input
-    |> Array.map getNumberOfWinningNumbers
+    |> Array.map getNumberOfWinningNumbersNew
 
 numberOfWinsPerCard
-|> Array.sumBy (fun x -> int (2. ** float (x - 1)))
+|> Array.sumBy (fun x -> pown 2 (x - 1))
 |> printfn "Part 1: %i"
 
 let cardCounts = Array.init input.Length (fun x -> x, 1) |> Map.ofArray
