d20305a4b28fb6b4f464d5327601770abf664cd4
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2023-12-04 20:17:13 +0200

    Cleanup

--- Day04.fsx
+++ Day04.fsx
@@ -1,32 +1,26 @@
 let input = System.IO.File.ReadAllLines("inputs/04.txt")
 #time
 
-let getNumberOfWinningNumbersNew (x: string) =
-    let split = x.Split([|"Card"; ":"; " "|], System.StringSplitOptions.RemoveEmptyEntries) |> Array.tail
-    let winning, my = split |> Array.splitAt (split |> Array.findIndex ((=) "|"))
-    Set.intersect (set winning) (set my)
-    |> Set.count
-
 let numberOfWinsPerCard = 
     input
-    |> Array.map getNumberOfWinningNumbersNew
+    |> Array.map (fun x ->
+        let split = x.Split([|"Card"; ":"; " "|], System.StringSplitOptions.RemoveEmptyEntries) |> Array.tail
+        let winning, my = split |> Array.splitAt (split |> Array.findIndex ((=) "|"))
+        Set.intersect (set winning) (set my)
+        |> Set.count
+    )
 
 numberOfWinsPerCard
 |> Array.sumBy (fun x -> pown 2 (x - 1))
 |> printfn "Part 1: %i"
 
-let cardCounts = Array.init input.Length (fun x -> x, 1) |> Map.ofArray
-
 numberOfWinsPerCard
 |> Array.indexed
 |> Array.fold (fun cards (card, wins) ->
-    let amountOfCards = Map.find card cards
-    Array.init wins ((+) (card + 1))
-    |> Array.fold (fun newC i ->
-        newC |> Map.change i (Option.map ((+) (1 * amountOfCards)))
-    ) cards
-) cardCounts
-|> Map.toSeq
-|> Seq.sumBy snd
+    [ card + 1 .. card + wins]
+    |> List.iter (fun j -> Array.tryItem j cards |> Option.iter (fun inst -> cards[j] <- inst + cards[card] ))
+    cards
+) (Array.create (Array.length numberOfWinsPerCard) 1)
+|> Array.sum
 |> printfn "Part 2: %i"
 #time
