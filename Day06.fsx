9eda7b9c0a6a7bf4a33810c10551157ad424fbea
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2023-12-06 12:54:42 +0200

    Use math instead of brute force

--- Day06.fsx
+++ Day06.fsx
@@ -1,7 +1,7 @@
 let input = System.IO.File.ReadAllLines("inputs/06.txt")
 #time
-let parse mapper chooser = input |> Array.map  (mapper >> Array.tail >> Array.map int64) |> chooser 
-let solve (t, d) = [|1L..t-1L|] |> Array.map (fun ms -> ms * (t - ms)) |> Array.filter ((<) d) |> Array.length
+let parse mapper chooser = input |> Array.map (mapper >> Array.tail >> Array.map int64) |> chooser 
+let solve (t, d) = ceil((float t+sqrt(pown (float t) 2-4.*float d))/2.-1.)-floor((float t-sqrt (pown (float t) 2-4.*float d))/2.+1.)+1. |> int64
 parse (fun (s: string) -> s.Split(" ", System.StringSplitOptions.RemoveEmptyEntries)) (fun ([|a;b|]) -> Array.zip a b) |> Array.map solve |> Array.reduce (*) |> printfn "Part 1: %i"
 parse (fun (s: string) -> s.Replace(" ", "").Split(":")) (fun ([|a;b|]) -> a[0], b[0]) |> solve |> printfn "Part 2: %i"
 #time
