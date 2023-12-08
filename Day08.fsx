17b8567e536234881456816cffe24cfa3fa25d3b
Valtteri Tarvus <valtteri.tarvus@gmail.com>
2023-12-08 08:26:22 +0200

    Small enhancements

--- Day08.fsx
+++ Day08.fsx
@@ -1,7 +1,7 @@
 #nowarn "25"
 let input = System.IO.File.ReadAllLines("inputs/08.txt")
 #time
-let steps = input[0]
+let steps = input[0] |> Seq.map (function 'L' -> fst | 'R' -> snd) |> Seq.toArray
 let stepsLength = steps.Length
 let directions =
     input[2..]
@@ -9,23 +9,20 @@ let directions =
     |> Array.map (fun parts -> parts.[0], (parts.[1], parts.[2]))
     |> Map.ofArray
 
-let rec calculateNeededSteps (i: int64) location  =
+let rec calculateNeededSteps i location  =
     let stepsTaken = i + 1L
-    let nextLocation = (match steps[int i%stepsLength] with 'L' -> fst | 'R' -> snd) directions.[location]
+    let nextLocation = steps[int i%stepsLength] directions[location]
     if nextLocation.EndsWith("Z") then
         stepsTaken
     else
         calculateNeededSteps stepsTaken nextLocation 
-let rec gcd a b =
-    if b = 0L then a else gcd b (a % b)
-let lcm (numbers: int64 array) =
-    numbers |> Array.fold (fun (acc: int64) x -> acc * x / (gcd acc x)) 1L
-
+let rec gcd a b = if b = 0L then a else gcd b (a % b)
+let lcm = Seq.reduce (fun acc x -> acc * x / (gcd acc x))
 let neededSteps = 
     directions
     |> Map.filter (fun key _ -> key.EndsWith("A"))
     |> Map.map (fun key _ -> calculateNeededSteps 0L key)
 
 neededSteps["AAA"] |> printfn "Part 1: %i"
-neededSteps |> Map.values |> Seq.toArray |> lcm |> printfn "Part 2: %i"
+neededSteps |> Map.values |> lcm |> printfn "Part 2: %i"
 #time
